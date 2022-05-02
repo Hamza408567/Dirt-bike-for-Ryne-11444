3ab9116d6a4dabcbc7dca48f82f86048e9893b57
HAMZAA\ah <ah408567@gmail.com>
2022-05-02 18:07:36 +0500

    adding minmap

---
+++ Assets/MinMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinMap : MonoBehaviour
+{
+    public Transform target; 
+    void Start()
+    {
+        target = Game_controller.instance.player.transform;
+    }
+
+    
+    void LateUpdate()
+    {
+        Vector3 newposition = target.position;
+        newposition.y = transform.position.y;
+        transform.position = newposition;
+        transform.rotation = Quaternion.Euler(90f, target.eulerAngles.y, 0f);
+    }
+}
